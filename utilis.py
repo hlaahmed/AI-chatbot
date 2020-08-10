af96bd99cebc590a503b9b9c1ad188a14adc2eca
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-10 08:00:28 +0200

    Update utilis.py

--- utilis.py
+++ utilis.py
@@ -1,20 +1,6 @@
 import app
+def save(response):
 
-
-def getPhone(phone):
-    if app.db.session.query(app.Users).filter(app.Users.phone == phone).count() == 0:
-        return 0
-    else:
-        return 1
-
-
-def save(name, phone, company, service):
-    if(str(company)=="etisalate"):
-        phone = int(str("011") + str(phone))
-    elif(str(company)=="orange"):
-        phone = int(str("012") + str(phone))
-    else:
-        phone = int(str("010") + str(phone))
-    data = app.Users(str(name), phone, str(company), str(service))
+    data = app.Users(response)
     app.db.session.add(data)
     app.db.session.commit()
